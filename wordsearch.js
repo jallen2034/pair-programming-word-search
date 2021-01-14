e27545525e580b3926278539c5329ecbbf2a7c27
Jacob Allen <jallen2033@gmail.com>
2021-01-14 02:28:33 +0000

    added wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
+//Pair programmed with Gabe, Idel, and Jacob
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    // check for rows
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
-}
+    // go through 2d array column by column and build a new array 
+    let newArray = [];
+    for (let i = 0; i < letters[0].length; i++) { 
+        let joinedColumn = [];
+        for (let j = 0; j < letters.length; j++) {
+            joinedColumn.push(letters[j][i]);
+        }
+        newArray.push(joinedColumn);
+    }
 
-module.exports = wordSearch
+    // newArray after join looks like this:
+    // newArray = ["ASYHWBUOE", "WEFMHFBDZ", "CICJCRTCK"]
+
+    // check for columns
+    const columnJoin = newArray.map(ls => ls.join(''));
+    for (l of columnJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
+};
+
+module.exports = wordSearch;
